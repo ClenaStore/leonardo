caa0e9736f60ae046d2eac8652cf5222590165b0
clena <storeclena@gmail.com>
2026-01-30 15:01:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,31 @@
-const CACHE_NAME = 'grupo-dv-v2';
+const CACHE_NAME = 'grupo-dv-pwa-v1';
 
-const FILES_TO_CACHE = [
+const FILES = [
   './app.html',
-  './dashboard.html',
-  './entradas.html',
-  './entradasanexo.html',
   './manifest.json',
   './leo.png'
 ];
 
-/* INSTALL */
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
 
-/* ACTIVATE */
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
+        keys.map(k => k !== CACHE_NAME && caches.delete(k))
       )
     )
   );
   self.clients.claim();
 });
 
-/* FETCH */
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
